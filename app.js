c4c2918794939d8018475c651a508ce932405850
DiaS793 <DiaScruggs793@outlook.com>
2022-04-02 01:12:56 -0400

    added the DOM

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+//DOM Objects
+const pokeName = document.querySelector('.poke-name');
+const pokeId = document.querySelector('.poke-id');
+const pokeFrontImage = document.querySelector('.poke-front-image');
+const pokeBackImage = document.querySelector('.poke-back-image');
+console.log(pokeName);
+console.log(pokeId);
+console.log(pokeFrontImage);
+console.log(pokeBackImage);
 
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+})
