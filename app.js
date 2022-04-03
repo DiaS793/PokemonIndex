b2a0040c4a448b299804da82d77377e93b625835
DiaS793 <DiaScruggs793@outlook.com>
2022-04-02 23:57:17 -0400

    update

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
 const pokeFrontImage = document.querySelector('.poke-front-image');
 const pokeBackImage = document.querySelector('.poke-back-image');
+const pokeTypeOne = document.querySelector('.poke-type-one');
+const pokeTypeTwo = document.querySelector('.poke-type-two');
+const pokeWeight = document.querySelector('.poke-weight');
+const pokeHeight = document.querySelector('.poke-height');
 console.log(pokeName);
 console.log(pokeId);
 console.log(pokeFrontImage);
 console.log(pokeBackImage);
+console.log(pokeTypeOne);
+console.log(pokeTypeTwo);
+console.log(pokeWeight);
+console.log(pokeHeight);
+
 
 fetch('https://pokeapi.co/api/v2/pokemon/1')
 .then(res => res.json())
