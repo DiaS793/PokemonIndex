8d9441a4c4cae3096bf3ed4e51c4032cbfc4e17b
DiaS793 <DiaScruggs793@outlook.com>
2022-04-06 00:00:35 -0400

    update 4/5/22 worked on .mainScreen and started on functions

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 //DOM Objects
+
+//FOR MAIN-SCREEN: This is a class that is apart of 
+//main-screen hide. The div and those in side that div hide
+//what is there. This also is a way to change the background.
+//take note that main-screen and hide are not connect via -.
+//this means that even though they are apart of one class they 
+//can affect or be affected differently.
+//REMEMBER: One way I have learned do create a grid is by
+//name a div .box a. Even though they are apart of one class 
+//because they are speared by a space, I can call one or the other
+//in css and manipulate them. This is the same here. 
+
 const mainScreen = document.querySelector('.main-screen');
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
@@ -8,39 +20,62 @@ const pokeTypeOne = document.querySelector('.poke-type-one');
 const pokeTypeTwo = document.querySelector('.poke-type-two');
 const pokeWeight = document.querySelector('.poke-weight');
 const pokeHeight = document.querySelector('.poke-height');
-// console.log(pokeName);
-// console.log(pokeId);
-// console.log(pokeFrontImage);
-// console.log(pokeBackImage);
-// console.log(pokeTypeOne);
-// console.log(pokeTypeTwo);
-// console.log(pokeWeight);
-// console.log(pokeHeight);
+
+// constants and variables
+const TYPES =[
+  'normal', 'fighting', 'flying', 'poison', 'ground',
+  'rock', 'bug','ghost','steel','fire','water','grass',
+  'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy'
+];
+
+//Functions
+
+const resetScreen =()=> {
+  for (const type of TYPES) {
+    console.log(type)
+    mainScreen.classList.remove(type);
+  }
+};
 
 fetch('https://pokeapi.co/api/v2/pokemon/1')
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
-    mainScreen.classList.remove('hide');
+
+    resetScreen();
+
+    const dataTypes = data['types'];
+    const dataFirstType = dataTypes[0];
+    const dataSecondType = dataTypes[1];
+    pokeTypeOne.textContent = dataFirstType['type']['name'];
+    if (dataSecondType) {
+      pokeTypeTwo.classList.remove('hide');
+      pokeTypeTwo.textContent = dataSecondType['type']['name'];
+    } else {
+      pokeTypeTwo.classList.add('hide');
+      pokeTypeTwo.textContent = '';
+    }
+
+    //Explained! So this works like this:
+    //in the api there is a type. Under that type there are
+    //two arrays for type: grass and poison.
+    //the mainScreen.classList.add looks for the FIRST type
+    //and matches that will the class that is in css and that 
+    //is what gives it the background color.
+
+    mainScreen.classList.add(dataFirstType['type']['name']);
+    
+
+    //gives this where the physical properties are brought in 
+    //for the pokemon.
     pokeName.textContent = data['name'];
     pokeId.textContent = data['id'];
     pokeWeight.textContent = data['weight'];
     pokeHeight.textContent = data['height'];
 
-    const dataTypes = data['types'];
-    const dataFirstType = dataTypes[0];
-    const dataSecondType = dataTypes[1];
-    pokeTypeOne.textContent = 
-    dataFirstType['type']['name'];
-    if (dataSecondType) {
-      pokeTypeTwo.classList.remove('hide');
-      pokeTypeTwo.textContent = 
-      dataSecondType['type']['name'];
-    } else {
-        pokeTypeTwo.classList.add('hide');
-        pokeTypeTwo.textContent='';
-    }
-
+    
+    //this is where the front and back images are brought in 
+    //for the pokemon
     pokeFrontImage.src = data['sprites']['front_default'] || '';
     pokeBackImage.src = data['sprites']['back_default'] || '';
   });
